2af7c7527f696aab140b15deb0755436475d8dcd
JesusxAguirre <99jesus.aguirre@gmail.com>
2025-03-18 12:27:39 -0400

    add compatibility with wide screens navigation rail recomended in material 3

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/router/destinations.dart';
 import 'package:movie_app/search/search_delegate.dart';
 
 import 'package:provider/provider.dart';
@@ -14,6 +15,9 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final navigationProvider = Provider.of<NavigationProvider>(context);
 
+    final size = MediaQuery.sizeOf(context);
+    bool wideScreen = size.width > 600;
+
     final Map<String, Widget> _screens = {
       AppNavigator.ROUTE_HOME: const HomeScreen(),
       AppNavigator.ROUTE_TV: TvScreen(),
@@ -21,29 +25,70 @@ class MainScreen extends StatelessWidget {
     };
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Conecta TV'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed:
-                () => showSearch(
-                  context: context,
-                  delegate: MovieSearchDelegate(),
-                ),
+      appBar:
+          wideScreen
+              ? null
+              : AppBar(
+                title: const Text('Conecta TV'),
+                actions: [
+                  IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed:
+                        () => showSearch(
+                          context: context,
+                          delegate: MovieSearchDelegate(),
+                        ),
+                  ),
+                ],
+              ),
+      body: Row(
+        children: [
+          // Add NavigationRail for wide screens
+          if (wideScreen)
+            NavigationRail(
+              leading: IconButton(
+                icon: Icon(Icons.search),
+                onPressed:
+                    () => showSearch(
+                      context: context,
+                      delegate: MovieSearchDelegate(),
+                    ),
+              ),
+              selectedIndex: _getIndex(navigationProvider.currentRoute),
+              minWidth: size.width * 0.01,
+              useIndicator: true,
+              groupAlignment: -1.0,
+              onDestinationSelected: (index) => _updateRoute(context, index),
+              labelType: NavigationRailLabelType.selected,
+              destinations:
+                  destinations.map((d) {
+                    return NavigationRailDestination(
+                      icon: Icon(d.icon),
+                      label: Text(d.label),
+                    );
+                  }).toList(),
+            ),
+          // Main content
+          Expanded(
+            child:
+                _screens[navigationProvider.currentRoute] ?? const HomeScreen(),
           ),
         ],
       ),
-      body: _screens[navigationProvider.currentRoute] ?? const HomeScreen(),
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: _getIndex(navigationProvider.currentRoute),
-        onDestinationSelected: (index) => _updateRoute(context, index),
-        destinations: const [
-          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-          NavigationDestination(icon: Icon(Icons.tv), label: 'TV'),
-          NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
-        ],
-      ),
+      bottomNavigationBar:
+          wideScreen
+              ? null
+              : NavigationBar(
+                selectedIndex: _getIndex(navigationProvider.currentRoute),
+                onDestinationSelected: (index) => _updateRoute(context, index),
+                destinations:
+                    destinations.map((d) {
+                      return NavigationDestination(
+                        icon: Icon(d.icon),
+                        label: d.label,
+                      );
+                    }).toList(),
+              ),
     );
   }
 
--- lib/screens/tv_screen.dart
+++ lib/screens/tv_screen.dart
@@ -9,48 +9,63 @@ class TvScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-
     final tvProvider = Provider.of<TvProvider>(context);
 
     final List<TvChannel> channels = tvProvider.tvChannels;
 
-    return Column(
-      children: [
-        SizedBox(
-          height: size.height * 0.3,
-          width: double.infinity,
-          child: FadeInImage(
-            fit: BoxFit.cover,
-            placeholder: AssetImage('assets/no-image.jpg'),
-            image: NetworkImage(
-              'https://www.rockaxis.com/img/newsList/3148230.jpg',
-            ),
-          ),
-        ),
-        const SizedBox(height: 5),
-        Expanded(
-          child: ListView.builder(
-            itemCount: channels.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                onTap: () {},
-                leading: Image.network(
-                  channels[index].FullPosterImg,
-                  width: 50, // Ancho de la imagen
-                  height: 50, // Alto de la imagen
-                  fit: BoxFit.cover, // Ajusta la imagen al espacio disponible
+    return Expanded(
+      child: ListView.builder(
+        itemCount: channels.length,
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return Column(
+              children: [
+                SizedBox(
+                  height: size.height * 0.6,
+                  width: double.infinity,
+                  child: FadeInImage(
+                    fit: BoxFit.cover,
+                    placeholder: AssetImage('assets/no-image.jpg'),
+                    image: NetworkImage(
+                      'https://www.rockaxis.com/img/newsList/3148230.jpg',
+                    ),
+                  ),
                 ),
 
-                trailing: Icon(
-                  Icons.arrow_forward_outlined,
-                  color: Colors.white,
+                const SizedBox(height: 5),
+                ListTile(
+                  onTap: () {},
+                  leading: Image.network(
+                    channels[index].FullPosterImg,
+                    width: 50,
+                    height: 50,
+                    fit: BoxFit.cover,
+                  ),
+
+                  trailing: Icon(
+                    Icons.arrow_forward_outlined,
+                    color: Colors.white,
+                  ),
+                  title: Text(channels[index].providerName),
                 ),
-                title: Text(channels[index].providerName),
-              );
-            },
-          ),
-        ),
-      ],
+              ],
+            );
+          }
+
+          return ListTile(
+            onTap: () {},
+            leading: Image.network(
+              channels[index].FullPosterImg,
+              width: 50, // Ancho de la imagen
+              height: 50, // Alto de la imagen
+              fit: BoxFit.cover, // Ajusta la imagen al espacio disponible
+            ),
+
+            trailing: Icon(Icons.arrow_forward_outlined, color: Colors.white),
+            title: Text(channels[index].providerName),
+          );
+        },
+      ),
     );
   }
 }
